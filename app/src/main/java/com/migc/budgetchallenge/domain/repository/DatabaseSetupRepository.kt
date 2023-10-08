f143782bbc09181fdd828a16b25efbd1cf5e6b19
Miguel <migc.dev@gmail.com>
2023-10-08 10:27:54 -0500

    added documentation

--- app/src/main/java/com/migc/budgetchallenge/domain/repository/DatabaseSetupRepository.kt
+++ app/src/main/java/com/migc/budgetchallenge/domain/repository/DatabaseSetupRepository.kt
@@ -2,8 +2,19 @@ package com.migc.budgetchallenge.domain.repository
 
 interface DatabaseSetupRepository {
 
+    /**
+     * Populates database with category values
+     */
     suspend fun setupCategories()
+
+    /**
+     * Populates database with user transaction values
+     */
     suspend fun setupUserTransactions()
+
+    /**
+     * Populates database with budget values
+     */
     suspend fun setupBudget()
 
 }
--- app/src/main/java/com/migc/budgetchallenge/domain/repository/UserTransactionRepository.kt
+++ app/src/main/java/com/migc/budgetchallenge/domain/repository/UserTransactionRepository.kt
@@ -6,8 +6,27 @@ import kotlinx.coroutines.flow.Flow
 
 interface UserTransactionRepository {
 
+    /**
+     * Retrieves user transaction values from database
+     * @param month         month
+     * @param year          year
+     * @return              a flow of a list of spending
+     */
     fun getUserTransactionsByDate(month: Int, year: Int): Flow<List<CategorySpending>>
+
+    /**
+     * Retrieves categories from database
+     * @return              a list of categories
+     */
     suspend fun getCategories(): List<Category>
+
+    /**
+     * Saves a new user transaction into database
+     * @param month         month
+     * @param year          year
+     * @param categoryId    id of category
+     * @param spent         amount spent
+     */
     suspend fun saveUserTransaction(month: Int, year: Int, categoryId: Int, spent: Double)
 
 }
